1006aeade088d695920a5f20d740b671383dd27d
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-11 21:29:03 +0300

    I Thank Ms . Shaima

--- Homework 1/Homework 1/ContentView.swift
+++ Homework 1/Homework 1/ContentView.swift
@@ -9,8 +9,203 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing:0){
+            Text("My Favorite Movies")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            ZStack{
+                
+                Color(.red)
+                
+                HStack{
+                    
+                    
+                    Image("AE").resizable()
+                        .scaledToFit()
+                        .frame( width: 150, height: 150)
+                    Spacer()
+
+                    
+                    Text("End Game ")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    //CR
+                    Spacer()
+                    
+                    
+                    Text("5").font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color.yellow)
+                        .padding(.trailing, 20.0)
+                    
+                    
+                }
+                
+                
+            }
+            ZStack{
+                
+                Color(.blue)
+                
+                HStack{
+                    
+                    
+                    Image("AI").resizable()
+                        .scaledToFit()
+                        .frame( width: 150, height: 150)
+                    Spacer()
+                    
+            
+                    Text("Infinity war ")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    //CR
+                    Spacer()
+                    
+                    
+                    Text("5.3").font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color.yellow)
+                        .padding(.trailing, 20.0)
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+            ZStack{
+                
+                Color(.green)
+                
+                HStack{
+                    
+                    
+                    Image("L").resizable()
+                        .scaledToFit()
+                        .frame( width: 150, height: 150)
+                    Spacer()
+                    
+            
+                    Text("Limitless ")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    //CR
+                    Spacer()
+                    
+                    
+                    Text("4.9").font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color.yellow)
+                        .padding(.trailing, 20.0)
+                    
+                    
+                }
+                
+                
+            }
+            
+            ZStack{
+                
+                Color(.purple)
+                
+                HStack{
+                    
+                    
+                    Image("WW").resizable()
+                        .scaledToFit()
+                        .frame( width: 150, height: 150)
+                    Spacer()
+                    
+            
+                    Text("The Wolf of Wall Street")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    //CR
+                    Spacer()
+                    
+                    
+                    Text("4").font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color.yellow)
+                        .padding(.trailing, 20.0)
+                    
+                    
+                }
+                
+                
+            }
+            ZStack{
+                
+                Color(.blue)
+                
+                HStack{
+                    
+                    
+                    Image("SM").resizable()
+                        .scaledToFit()
+                        .frame( width: 150, height: 150)
+                    Spacer()
+                    
+            
+                    Text("Spider-Man ")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    
+                    //CR
+                    Spacer()
+                    
+                    
+                    Text("3.2").font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color.yellow)
+                        .padding(.trailing, 20.0)
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+            
+        }
     }
 }
 
